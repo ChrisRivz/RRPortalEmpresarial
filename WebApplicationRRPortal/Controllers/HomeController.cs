4fd4e1cf1a4923350ecf2d995df853d1670602ae
davidIslas10 <124799361+davidIslas10@users.noreply.github.com>
2023-04-11 17:58:50 -0600

    New Model

--- WebApplicationRRPortal/Controllers/HomeController.cs
+++ WebApplicationRRPortal/Controllers/HomeController.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using Microsoft.Ajax.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApplicationRRPortal.Controllers
 {
     public class HomeController : Controller
     {
+        
+
         public ActionResult Index()
         {
             return View();
@@ -22,9 +28,15 @@ namespace WebApplicationRRPortal.Controllers
 
         public ActionResult Contact()
         {
+          
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
+
+        public bool Test()
+        {
+            using (var context = new RRUATEntities)
+        }
+
     }
 }
