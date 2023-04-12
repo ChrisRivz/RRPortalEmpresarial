981b9d0d67dafae9240b708956e23147803f2d1f
davidIslas10 <124799361+davidIslas10@users.noreply.github.com>
2023-04-12 14:13:35 -0600

    Deleting using

--- WebApplicationRRPortal/Controllers/HomeController.cs
+++ WebApplicationRRPortal/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebGrease.Css.Ast.Selectors;
-using WebApplicationRRPortal.UsersModels;
 
 namespace WebApplicationRRPortal.Controllers
 {
